47005cc75b83a24e1ac397eace2ebeb2cf457e28
Lukas Motiejūnas <motiejunas.lukas@gmail.com>
2019-02-11 18:48:00 +0200

    add vagrant

--- src/server/dev.js
+++ src/server/dev.js
@@ -8,49 +8,49 @@ import 'ignore-styles';
 import devConfig from '../../webpack.dev.config';
 
 function normalizeAssets(assets) {
-  if (isObject(assets)) {
-    return Object.values(assets);
-  }
-  return Array.isArray(assets) ? assets : [assets];
+    if (isObject(assets)) {
+        return Object.values(assets);
+    }
+    return Array.isArray(assets) ? assets : [assets];
 }
 
 const compiler = webpack(devConfig);
 const app = express();
 
 app.use(
-  devMiddleware(compiler, {
-    serverSideRender: true
-  })
+    devMiddleware(compiler, {
+        serverSideRender: true
+    })
 );
 
 app.use(
-  hotMiddleware(compiler, {
-    serverSideRender: true
-  })
+    hotMiddleware(compiler, {
+        serverSideRender: true
+    })
 );
 
 app.get('/', (req, res) => {
-  const assetsByChunkName = res.locals.webpackStats.toJson().assetsByChunkName;
-  const fs = res.locals.fs;
-  const outputPath = res.locals.webpackStats.toJson().outputPath;
+    const assetsByChunkName = res.locals.webpackStats.toJson().assetsByChunkName;
+    const fs = res.locals.fs;
+    const outputPath = res.locals.webpackStats.toJson().outputPath;
 
-  res.send(`
+    res.send(`
 <html>
   <head>
     <title>Kayak UI Academy</title>
     <style>
 		${normalizeAssets(assetsByChunkName.main)
-      .filter(path => path.endsWith('.css'))
-      .map(path => fs.readFileSync(outputPath + '/' + path))
-      .join('\n')}
+        .filter(path => path.endsWith('.css'))
+        .map(path => fs.readFileSync(outputPath + '/' + path))
+        .join('\n')}
     </style>
   </head>
   <body>
     <div id="root"></div>
 		${normalizeAssets(assetsByChunkName.main)
-      .filter(path => path.endsWith('.js'))
-      .map(path => `<script src="${path}"></script>`)
-      .join('\n')}
+        .filter(path => path.endsWith('.js'))
+        .map(path => `<script src="${path}"></script>`)
+        .join('\n')}
   </body>
 </html>
   `);
